ec03f900de2ccea11e274c2495e369497b18f593
Parteek Saini <sainiparteek18@gmail.com>
2022-06-08 00:02:31 -0700

    added profile page

---
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,79 @@
+import React from "react";
+import "./Profile.css";
+import Typical from "react-typical";
+
+function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icons">
+              <a
+                href="https://www.facebook.com/parteek.saini.756/"
+                target="_blank"
+              >
+                <i className="fa fa-facebook-square"></i>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/parteek-saini-95a122158/"
+                target="_blank"
+              >
+                <i className="fa fa-linkedin"></i>
+              </a>
+              <a href="https://github.com/ParteekSaini" target="_blank">
+                <i className="fa fa-github"></i>
+              </a>
+              <a href="#" target="_blank">
+                <i className="fa fa-instagram"></i>
+              </a>
+            </div>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'M <span className="highlighted-text">Parteek</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                {" "}
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Enthusiastic Dev 🔴",
+                    1000,
+                    "Full Stack Developer 💻",
+                    1000,
+                    "MERN Stack Dev 😎",
+                    1000,
+                  ]}
+                />
+              </h1>
+              <span className="profile-role-tagline">
+                Knack of building applications with front and back end
+                operations.
+              </span>
+            </span>
+          </div>
+          <div className="profile-options">
+            <button className="btn primary-btn">
+              {""}
+              Hire Me{" "}
+            </button>
+            <a href="ParteekResume.pdf" download="Parteek Resume.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
